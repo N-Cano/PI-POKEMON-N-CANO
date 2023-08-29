9983e9fb51fd2f387c12c0fa0c9e0fc295d0d867
N-Cano <nahuelcano2009@gmail.com>
2023-08-29 17:03:13 -0300

    avances

---
+++ api/src/utils/globals.js
@@ -0,0 +1,9 @@
+const API_POKE = "https://pokeapi.co/api/v2/pokemon?limit=40";
+const API_POKE_TYPE = "https://pokeapi.co/api/v2/type";
+const API_POKE_NAME_OR_ID = "https://pokeapi.co/api/v2/pokemon/";
+
+module.exports = {
+    API_POKE,
+    API_POKE_TYPE,
+    API_POKE_NAME_OR_ID
+}
