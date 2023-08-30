7faadfdc02d9b67b85b160f88e420b2f845dce2c
N-Cano <nahuelcano2009@gmail.com>
2023-08-30 20:11:09 -0300

    Comienzo del Front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
-
+import Home from "./views/Home/Home";
+import Create from "./views/Create/Create";
+import Landing from "./views/Landing/Landing";
+import Detail from "./views/Detail/Detail";
+import { Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <Route exact path='/' component={Landing} />
+      <Route exact path='/home' component={Home} />
+      <Route exact path='/detail/:id' component={Detail} />
+      <Route exact path='/create' component={Create} />
     </div>
   );
 }
---
+++ client/src/views/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Esta es la vita de Home</h1>
+        </div>
+    )
+}
+
+export default Home
