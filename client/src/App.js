8a8ac339af079b74546b19bd7aa605a95a3e3ac1
N-Cano <nahuelcano2009@gmail.com>
2023-09-13 15:03:55 -0300

    Se eliminaron elementos innecesarios

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
-import { Route, useLocation } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import { Home, Landing, Detail, Create } from "./views";
-import NavBar from './components/NavBar/NavBar';
 
 import './App.css';
 
 function App() {
 
-  const location = useLocation();
-
   return (
     <main className="App">
-
-      {/* {location.pathname !== '/' && <NavBar />} */}
 
       <Route exact path='/' component={Landing} />
       <Route exact path='/home' component={Home} />
