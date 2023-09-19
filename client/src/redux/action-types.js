d989e78bf641bb6a9090383d79a1b5980faf8008
N-Cano <nahuelcano2009@gmail.com>
2023-09-19 18:23:21 -0300

    Borrado de espacios

--- client/src/redux/action-types.js
+++ client/src/redux/action-types.js
@@ -7,5 +7,4 @@ export const FILTER_DBAPI = 'FILTER_DBAPI';
 export const ORDER_BY_NAME = 'ORDER_BY_NAME';
 export const ORDER_BY_ATTACK = 'ORDER_BY_ATTACK';
 export const GET_POKEMON_TYPE = 'GET_POKEMON_TYPE';
-export const GET_POKEMON_IMG = 'GET_POKEMON_IMG'
-
+export const GET_POKEMON_IMG = 'GET_POKEMON_IMG';
